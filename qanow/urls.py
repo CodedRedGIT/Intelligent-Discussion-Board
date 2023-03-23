5c36a46ea5a31312fb761bb4a68d5edcb7711c3c
CodedRed <devcodedred@gmail.com>
2023-03-23 14:19:25 -0500

    added login api

--- qanow/urls.py
+++ qanow/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('questions/', views.get_all_questions, name='all_questions'),
     path('questions/<str:id>/', views.get_question, name='get_question'),
+    path('login/', views.login, name='login_api'),
 ]
--- qanow/views.py
+++ qanow/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate, login
 
 from .models import Question
 from .serializer import QuestionSerializer
@@ -17,3 +18,14 @@ def get_question(request, id):
     questions = Question.objects.get(id=id)
     serializer = QuestionSerializer(questions, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response({'message': 'Login successful'})
+    else:
+        return Response({'error': 'Invalid credentials'}, status=400)
