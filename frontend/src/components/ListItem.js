6dcb63a4fd2fec9057f0abffd9a75c9ba0bcb54b
CodedRed <devcodedred@gmail.com>
2023-03-29 19:25:49 -0500

    Updated frontend to use new Models/endpoints

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const ListItem = ({ question }) => {
-  const date = question.published_date.substring(0, 10);
-  const time = question.published_date.substring(11, 16);
+const ListItem = ({ post }) => {
+  const date = post.published_date.substring(0, 10);
+  const time = post.published_date.substring(11, 16);
 
   return (
-    <Link to={`/questions/${question.id}`}>
-      <h3>{question.prompt}</h3>
+    <Link to={`/posts/${post.id}`}>
+      <h3>{post.prompt}</h3>
       <div className="thread__info">
-        <small>{question.tag}</small>
+        <small>{post.tag}</small>
         <small>{date}</small>
         <small>{time}</small>
       </div>
