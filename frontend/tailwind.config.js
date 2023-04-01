22e64f3d2a8243531acf50c9176ec483857de00e
CodedRed <devcodedred@gmail.com>
2023-03-31 19:31:37 -0500

    restructured project with yarn & eslint

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,7 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx}", "./components/**/*.{js,ts,jsx}"],
   theme: {
     extend: {
       fontFamily: {
