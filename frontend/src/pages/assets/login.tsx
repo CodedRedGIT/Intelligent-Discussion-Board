4652890c7681c3c4928e1605476334521054d05a
CodedRed <devcodedred@gmail.com>
2023-04-02 21:30:16 -0500

    updated login page

--- frontend/src/pages/assets/login.tsx
+++ frontend/src/pages/assets/login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import useLogin from '../api/useLogin'
+import { Card } from '@/components/layout/Card'
 
 const Login = () => {
   const [email, setEmail] = useState('')
@@ -14,48 +15,52 @@ const Login = () => {
   }
 
   return (
-    <div className='login flex flex-col justify-center items-center h-full'>
-      <h1 className='loginTitle text-center mb-8'>Log In</h1>
-      <form className='loginForm flex flex-col w-70' onSubmit={handleSubmit}>
-        <label htmlFor='email' className='mb-2'>
-          Email Address
-        </label>
-        <input
-          required
-          placeholder='Email Address'
-          type='email'
-          name='email'
-          id='email'
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          className='py-2 px-4 border border-gray-300 rounded-lg mb-5'
-        />
-        <label htmlFor='password' className='mb-2'>
-          Password
-        </label>
-        <input
-          required
-          placeholder='Password'
-          type='password'
-          name='password'
-          id='password'
-          minLength={6}
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          className='py-2 px-4 border border-gray-300 rounded-lg mb-5'
-        />
-        <button
-          className='loginBtn bg-purple-500 text-white py-2 px-4 rounded-lg font-bold'
-          disabled={loading}
-        >
-          {loading ? 'Loading...' : 'Sign In'}
-        </button>
-      </form>
-      <h3 className='text-center mt-8'>
-        Don&apos;t have an account?&nbsp;
-        <Link href='/register'>Register</Link>
-      </h3>
-      {error && <p>{error}</p>}
+    <div className='flex flex-col justify-center items-center h-full'>
+      <Card className='w-96'>
+        <h1 className='text-center mb-8 text-2xl'>Log In</h1>
+        <form className='flex flex-col' onSubmit={handleSubmit}>
+          <label htmlFor='email' className='mb-2 text-xl'>
+            Email Address
+          </label>
+          <input
+            required
+            placeholder='Email Address'
+            type='email'
+            name='email'
+            id='email'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            className='py-2 px-4 border border-gray-300 rounded-lg mb-5 text-xl'
+          />
+          <label htmlFor='password' className='mb-2 text-xl'>
+            Password
+          </label>
+          <input
+            required
+            placeholder='Password'
+            type='password'
+            name='password'
+            id='password'
+            minLength={6}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            className='py-2 px-4 border border-gray-300 rounded-lg mb-5 text-xl'
+          />
+          <button
+            className=' bg-green-500 text-white py-2 px-4 rounded-lg font-bold text-xl'
+            disabled={loading}
+          >
+            {loading ? 'Loading...' : 'Sign In'}
+          </button>
+        </form>
+        <h3 className='text-center mt-8 text-xl'>
+          Don&apos;t have an account?&nbsp;
+          <Link href='/register' className='underline text-blue-500'>
+            Register
+          </Link>
+        </h3>
+        {error && <p className='text-red-500 mt-4 text-lg'>{error}</p>}
+      </Card>
     </div>
   )
 }
