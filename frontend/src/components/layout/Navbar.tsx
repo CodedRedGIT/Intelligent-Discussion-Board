ad29e4693f02384fb6dc38d8882aa02d9831f067
CodedRed <devcodedred@gmail.com>
2023-04-25 20:46:39 -0500

    fixed navbar at different screen sizes

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   return (
     <nav className='bg-amber-500 fixed top-0 left-0 border-b-2 border-gray-400 z-40 flex w-full justify-between h-32'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-start justify-between w-full'>
-        <div className='mt-10 ml-[-75px] pl-2 px-4'>
+        <div className='mt-10 fixed left-50'>
           <FontAwesomeIcon
             icon={faHouse}
             className='fa-solid text-white text-4xl cursor-pointer'
@@ -30,15 +30,18 @@ const Navbar = () => {
           />
         </div>
 
-        <div className='text-white text-5xl font-bold text-center flex-grow mt-10'>
+        <div
+          className='text-white font-bold text-center justify-center flex-grow mt-6'
+          style={{ fontSize: 'clamp(2rem, 3vw, 5rem)' }}
+        >
           Intelligent Discussion Board
         </div>
 
-        <div>
+        <div className='fixed right-0 mr-4 mt-10'>
           {sessionData ? (
             <button
               onClick={handleSignOut}
-              className='bg-green-500 px-4 py-2 text-white rounded-lg !important mt-10'
+              className='bg-green-500 px-4 py-2 text-white rounded-lg !important'
             >
               Sign Out
             </button>
